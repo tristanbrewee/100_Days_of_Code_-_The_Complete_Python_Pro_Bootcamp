a57a2b852b3fc6f556d68092a5b063f83278f6cb
tristan <tristanbrewee@gmail.com>
2026-01-18 20:53:30 +0100

    Added "Variables"-Excercise (glasses)

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -4,3 +4,11 @@ print("Your name is", name)
 print("Your length is ", length)
 
 print("Your Length is: " + len(input("What is your name?")).__str__())
+
+#Variables : Part two
+glass1 = "milk"
+glass2 = "juice"
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
