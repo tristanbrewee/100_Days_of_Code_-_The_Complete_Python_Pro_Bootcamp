01af6583ad81b26cd3cd3fa3494ae6a7adec90de
tristan <tristanbrewee@gmail.com>
2026-01-18 20:48:55 +0100

    Added "Variables"-Excercise

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,6 @@
-print("Hello " + input("What is your name? ") +"!")
+name = input("Enter your name: ")
+length = len(name)
+print("Your name is", name)
+print("Your length is ", length)
+
+print("Your Length is: " + len(input("What is your name?")).__str__())
