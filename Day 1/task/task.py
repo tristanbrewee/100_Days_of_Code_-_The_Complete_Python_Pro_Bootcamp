b9b202103917c59c39d99a56c2a47cbc81e01d98
tristan <tristanbrewee@gmail.com>
2026-01-18 19:23:40 +0100

    Updates bug in "square-code" where a value was hardcoded instead of dynamically

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -2,17 +2,14 @@
 print("Hello, my name is Tristan.")
 
 number = int (input("How many columns?"))
-number *= 3
+number *= 5
 for index in range(0, number + 1):
     for index2 in range(0, number + 1):
         if index % 5 == 0:
             print("*", end=" ")
         elif(index2 % 5 == 0):
             print("*", end=" ")
-
         elif (index % 5 != 0):
             print(" ", end=" ")
-
-
-        if (index2 == 15):
+        if (index2 == number):
             print("")
