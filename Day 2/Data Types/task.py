6884902c8ccbc4c5dd688cf9918bf335d18d7196
tristan <tristanbrewee@gmail.com>
2026-01-19 19:55:42 +0100

    Day 2 lesson 2 added

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,18 @@
-len("Hello")
+#Subscripting
+print("Hello"[-1])
+
+#Strings
+print("123" + "345")
+
+#Integer = Whole Number
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Float = Floating Point Number
+print(3141.59)
+
+#Boolean
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,12 @@
-len(12345)
+# len("Hello")
+#
+# print(type(len("Hello")))
+#
+# print(type("String"))
+# print(type(123))
+# print(type(123.456))
+# print(type(True))
+
+print(int("123") + int("456"))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
