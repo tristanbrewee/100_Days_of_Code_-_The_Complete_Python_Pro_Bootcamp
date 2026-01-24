4a54de0a625cc671e1709cff192d9fc187738009
tristan <tristanbrewee@gmail.com>
2026-01-24 13:43:32 +0100

    Highest Score Added

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,18 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+
+total_exam_score = sum(student_scores)
+print(total_exam_score)
+
+total_exam_score = 0
+for score in student_scores:
+    total_exam_score += score
+print(total_exam_score)
+
+highest_score = max(student_scores)
+print(highest_score)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(highest_score)
